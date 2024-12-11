f2e425a3826007e0209bfd71e6aa5e3d156c488e
ana <ana.sacramento@musicpro.live>
2024-12-11 16:09:42 -0300

    Controle financeiro adicinado

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledFooter } from './styles'
+
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      Desenvolvido com 🤍 por Ana Santos, sob mentoria de Amanda Silva!
+    </StyledFooter>
+  );
+};
+
+export default Footer;
