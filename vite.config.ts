b0403f1d83da81c4287a3838ee0e5de20a67fc9b
ana <ana.sacramento@musicpro.live>
2025-01-19 12:51:53 -0300

    Ajustes no projeto e correções de erro

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Adicionando alias para react-firebase-hooks
+      'react-firebase-hooks': path.resolve(__dirname, 'node_modules/react-firebase-hooks'),
+    },
+  },
 });
